c6fed60b0fb307dc8099a9a4699cec4da0f62599
Patrick Luong <pluonggr@srv603.webhostingforstudents.com>
2022-05-19 13:48:22 -0400

    added controller rewrote index

--- model/validation.php
+++ model/validation.php
@@ -5,19 +5,23 @@
  * Validate user input from the diner app
  *
  */
-
-// Food must have at least 2 characters
-function validFood($food){
-/*    if (strlen(trim($food)) >= 2){
-        return true;
-    } else {
-        return false;
+class Validation
+{
+    // Food must have at least 2 characters
+    static function validFood($food){
+        /*    if (strlen(trim($food)) >= 2){
+                return true;
+            } else {
+                return false;
+            }
+        */
+        return strlen(trim($food)) >= 2;
     }
-*/
-    return strlen(trim($food)) >= 2;
+
+    //Validate meal
+    static function validMeal($meal){
+        return in_array($meal, DataLayer::getMeals());
+    }
+
 }
 
-//Validate meal
-function validMeal($meal){
-    return in_array($meal, getMeals());
-}
